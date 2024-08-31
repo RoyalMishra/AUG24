52c87fe9d062489df5041798626f3d5267974772
royal <royalmishr@gmail.com>
2024-08-31 15:07:38 +0530

    Css rm

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,53 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Page</title>
 </head>
-body {
-    background-color: khaki;
-    font-size: x-large;
-    text-align: center;
-    font-synthesis-position: revert-layer;
-    font-style: normal;
-    text-decoration-color: #2ca67d;
-    margin-bottom: inherit;
-    height: 183.367px;
-    width: 1542px;
-    margin-top: 15px;
-    border-bottom-width: 34px;
-    border-bottom-style: solid;
-    border-left-width: 27px;
-    border-left-style: solid;
-    border-right-width: -45px;
-    border-right-style: solid;
-    border-top-width: 31px;
-    border-top-style: solid;
-    margin-left: 15px;
-    margin-right: 0px;
-  }
-<body class="body">
 
-body {
-    background-color: khaki;
-    font-size: x-large;
-    text-align: center;
-    font-synthesis-position: revert-layer;
-    font-style: normal;
-    text-decoration-color: #2ca67d;
-    margin-bottom: inherit;
-    height: 183.367px;
-    width: 1542px;
-    margin-top: 15px;
-    border-bottom-width: 34px;
-    border-bottom-style: solid;
-    border-left-width: 27px;
-    border-left-style: solid;
-    border-right-width: -45px;
-    border-right-style: solid;
-    border-top-width: 31px;
-    border-top-style: solid;
-    margin-left: 15px;
-    margin-right: 0px;
-  }
-  
+<body bgcolor="pink">
 >
     <h1>This is the Contact page</h1>
     <a href="/about">About us</a>
