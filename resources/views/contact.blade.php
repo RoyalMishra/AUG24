82e30a615cef6a33d5b671075542a0d632bd8893
royal <royalmishr@gmail.com>
2024-09-03 12:42:16 +0530

    New

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home Page</title>
+    <title>Contact Page</title>
 </head>
 
 <body bgcolor="pink">
->
+
     <h1>This is the Contact page</h1>
     <a href="/about">About us</a>
     <a href="/contact">Contact</a>
