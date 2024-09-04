7266cd0708b0239c5101867cbe286f02f17ad512
royal <royalmishr@gmail.com>
2024-09-04 19:54:09 +0530

    New Layout Dir

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home Page</title>
-</head>
-<body>
+<components-layout>
+
     <h1>This is the Home page</h1>
-    <a href="/about">About us</a>
-    
-    <a href="/contact">Contact</a>
-    <a href="/home">Home Page</a>
-</body>
-</html>
+
+</components-layout>   
